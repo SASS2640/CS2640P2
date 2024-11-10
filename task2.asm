d04c517a6fc2b213d9c997eff9ce08062c9af1e9
SarahAHuynh <shuynh1@outlook.com>
2024-11-10 14:55:49 -0800

    Initial commit

---
+++ task2.asm
@@ -0,0 +1,19 @@
+#CS 2640
+#11/10/2024
+#Sarah Huynh, Sayumi Amarasinghe, Ashley Mapes, Samuel Ji
+#Program 2: Practice with Conditional and Loops
+#Task 2
+#Goals:
+# 1) take in two ints from a user (x and y)
+# 2) make sure user will know which value is 'x' and 'y'
+# 3) find the result of 'x' to the power of 'y'
+# 4) output the result to the user
+# 5) include a main lable, a looping label, loop counter, and an exit label
+
+.data
+
+
+.text
+
+
+main:

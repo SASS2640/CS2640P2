5102175437e559d4063a7e27907f9b662358d574
SarahAHuynh <shuynh1@outlook.com>
2024-11-10 21:34:00 -0800

    implemented power algorithm and output

--- task2.asm
+++ task2.asm
@@ -11,22 +11,22 @@
 # 5) include a main label, a looping label, loop counter, and an exit label
 
 .data  
-prompt1: .asciiz "This program asks the user to input a value for 'x' and 'y'. Then, it finds the value of x to the power of y. For example, 2 to the power 3 is 8.\n"
-prompt2: .asciiz " Enter a number for 'x': "
-prompt3: .asciiz " Enter a number for 'y': "
+prompt: .asciiz "This program asks the user to input a value for 'x' and 'y'.\nThen, it finds the value of x to the power of y.\nFor example, 2 to the power 3 is 8.\n"
+promptInputX: .asciiz "Enter a number for 'x': "
+promptInputY: .asciiz "Enter a number for 'y': "
+result: .asciiz "'x' to the power 'y' is: " 
 newline: .asciiz "\n" 
-
 
 .text 
 main: 
-
+	#print out prompt
 	li $v0, 4 
-	la $a0, prompt1 
+	la $a0, prompt 
 	syscall 
 	
-	#print out prompt2
+	#print out promptInputX
 	li $v0, 4 
-	la $a0, prompt2 
+	la $a0, promptInputX 
 	syscall 
 	
 	#read x value from user 
@@ -34,9 +34,9 @@ main:
 	syscall 
 	move $s0, $v0 
 	
-	#print out x value 
+	#print out promptInputY 
 	li $v0, 4 
-	la $a0, prompt3
+	la $a0, promptInputY
 	syscall 
 	
 	#read y value from user 
@@ -44,6 +44,36 @@ main:
 	syscall 
 	move $s1, $v0
 	
+	#initialize result to 1 (x^0 = 1)
+	li $t0, 1
+	
+	#initialize loop counter to y value
+	move $t1, $s1
+
+loop:
+	#if counter is 0, go to exit
+	beq $t1, 0, exit
+	#result *= x
+	mul $t0, $t0, $s0
+	#counter -= 1
+	sub $t1, $t1, 1
+	j loop
+
+exit:
+	#print result message
+	li $v0, 4
+	la $a0, result
+	syscall
+	
+	#print the result value
+	move $a0, $t0
+	li $v0, 1
+	syscall
+	
+	#print newline
+	li $v0, 4
+	la $a0, newline
+	syscall
 	
 	#exit program
 	li $v0, 10 

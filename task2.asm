f1a9b75903214a63de8ec78cf11a0bd43c231d2a
SarahAHuynh <shuynh1@outlook.com>
2024-11-10 21:12:22 -0800

    implemented task 2 user input

--- task2.asm
+++ task2.asm
@@ -8,12 +8,43 @@
 # 2) make sure user will know which value is 'x' and 'y'
 # 3) find the result of 'x' to the power of 'y'
 # 4) output the result to the user
-# 5) include a main lable, a looping label, loop counter, and an exit label
+# 5) include a main label, a looping label, loop counter, and an exit label
 
-.data
+.data  
+prompt1: .asciiz "This program asks the user to input a value for 'x' and 'y'. Then, it finds the value of x to the power of y. For example, 2 to the power 3 is 8.\n"
+prompt2: .asciiz " Enter a number for 'x': "
+prompt3: .asciiz " Enter a number for 'y': "
+newline: .asciiz "\n" 
 
 
-.text
+.text 
+main: 
 
-
-main:
+	li $v0, 4 
+	la $a0, prompt1 
+	syscall 
+	
+	#print out prompt2
+	li $v0, 4 
+	la $a0, prompt2 
+	syscall 
+	
+	#read x value from user 
+	li $v0, 5 
+	syscall 
+	move $s0, $v0 
+	
+	#print out x value 
+	li $v0, 4 
+	la $a0, prompt3
+	syscall 
+	
+	#read y value from user 
+	li $v0, 5
+	syscall 
+	move $s1, $v0
+	
+	
+	#exit program
+	li $v0, 10 
+	syscall 
